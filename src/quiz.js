17a09210a9e12c2bc2da655a0626ba51521c8c68
amartya-rs <saha.amartya@outlook.com>
2021-12-03 09:48:15 +0530

    refactor: user score logic

--- src/quiz.js
+++ src/quiz.js
@@ -2,10 +2,23 @@ var button = document.querySelector(".check-button");
 var outputDiv = document.querySelector(".output");
 var form = document.querySelector(".form-quiz");
 
-const correctAnswers = ["90", "Right Angled"];
+const correctAnswers = ["90", "Right Angle"];
+
 
 function onClickHandler() {
-  console.log("click");
+  var score = 0;
+  var index = 0;
+
+  var formDatas = new FormData(form);
+  for(let value of formDatas.values()) {
+    if (value == correctAnswers[index]) {
+      score++
+    }
+    index++
+  }
+
+  outputDiv.innerText = "Your score is: " + score;
 }
+
 
 button.addEventListener("click", onClickHandler);
