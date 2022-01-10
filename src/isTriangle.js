99f70ffc46a7f60f7870d2e85bb53c9147eb2dd8
amartya-rs <saha.amartya@outlook.com>
2022-01-10 13:48:20 +0530

    refactor: update ES6+ syntax

--- src/isTriangle.js
+++ src/isTriangle.js
@@ -1,17 +1,19 @@
-var inputAngle = document.querySelectorAll("input");
-var checkbutton = document.querySelector(".check-button");
-var output = document.querySelector(".output");
+const inputAngle = document.querySelectorAll("input");
+const checkbutton = document.querySelector(".check-button");
+const output = document.querySelector(".output");
 
-checkbutton.addEventListener("click", clickHandler);
 
-function clickHandler() {
-  var sum = 0;
-  for (var i = 0; i < inputAngle.length; i++) {
+let clickHandler = () => {
+  let sum = 0;
+  for (let i = 0; i < inputAngle.length; i++) {
     sum = sum + Number(inputAngle[i].value);
   }
   if (sum === 180) {
     output.innerText = "Yayy, the angles form a triangle";
-  } else {
+  } 
+  else {
     output.innerText = "Oh no, the angles do not form a triangle";
   }
 }
+
+checkbutton.addEventListener("click", clickHandler);
