82acf5ecb6f24396b40d2b4ac90d0601220a8457
amartya-rs <saha.amartya@outlook.com>
2022-01-10 13:44:41 +0530

    refactor: update ES6+ syntax

--- src/area.js
+++ src/area.js
@@ -1,10 +1,11 @@
-var dimension = document.querySelectorAll("input");
-var checkbutton = document.querySelector(".check-button");
-var output = document.querySelector(".output");
+const dimension = document.querySelectorAll("input");
+const checkbutton = document.querySelector(".check-button");
+const output = document.querySelector(".output");
+
+
+let clickHandler = () => {
+  const area = 0.5 * Number(dimension[0].value) * Number(dimension[1].value);
+  output.innerText = `Area of the triangle is ${area} cm²`;
+}
 
 checkbutton.addEventListener("click", clickHandler);
-
-function clickHandler() {
-  var area = 0.5 * Number(dimension[0].value) * Number(dimension[1].value);
-  output.innerText = "Area of the triangle is " + area + "cm²";
-}
--- src/hypotenuse.js
+++ src/hypotenuse.js
@@ -1,21 +1,22 @@
-var inputSides = document.querySelectorAll("input");
-var checkbutton = document.querySelector(".check-button");
-var output = document.querySelector(".output");
+const inputSides = document.querySelectorAll("input");
+const checkbutton = document.querySelector(".check-button");
+const output = document.querySelector(".output");
 
-checkbutton.addEventListener("click", clickHandler);
 
-function clickHandler() {
-  var array = [""];
-  for (var i = 0; i < inputSides.length; i++) {
+
+let clickHandler = () => { 
+  let array = [""];
+  for (let i = 0; i < inputSides.length; i++) {
     array[i] = Number(inputSides[i].value);
   }
 
   result(array);
 }
 
-function result(array) {
-  var side1 = Math.pow(array[0], 2);
-  var side2 = Math.pow(array[1], 2);
-  output.innerText =
-    "Length of hypotenuse is " + Math.sqrt(side1 + side2) + " cm";
+let result = array => {
+  const side1 = Math.pow(array[0], 2);
+  const side2 = Math.pow(array[1], 2);
+  output.innerText = `Length of hypotenuse is ${Math.sqrt(side1 + side2)} cm`;
 }
+
+checkbutton.addEventListener("click", clickHandler); //code works even if we move this line above result function. Understand the flow of code.
